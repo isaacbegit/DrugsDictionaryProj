e40c8a1cd062855c975b882b42fe58048afa9113
Isaac Bebawy <esaaco@live.com>
2019-11-30 15:42:24 +0200

    good

---
+++ DrugsDictionaryProj/Classes/HelperClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class HelperClass
+    {
+
+    //Importing Data From SQL Server--> Save it to SQlite database
+
+    public static string  Imprting_Data()
+    {
+
+        string connString = @"Data Source="+@"DELL-ISAACBE\SQLEXPRESS2017" +"; Initial Catalog=Drugs_DB"+
+                   "; Persist Security Info=True;MultipleActiveResultSets=true;User ID=admin;Password=admin";
+
+        DataTable _dt = new DataTable();
+
+
+        _dt.Columns.Add("DrugArName");
+        _dt.Columns.Add("DrugEnName");
+        _dt.Columns.Add("DrugID");
+        _dt.Columns.Add("ParentDrugArName");
+        _dt.Columns.Add("ParentDrugEnName");
+
+
+       
+        string query = "select  DrugArName,"+
+      "DrugEnName,"+
+      "DrugID,"+
+      "ParentDrugArName,"+
+      "ParentDrugEnName  from tblDrugBrandNames";
+
+        SqlConnection conn = new SqlConnection(connString);
+        SqlCommand cmd = new SqlCommand(query, conn);
+        conn.Open();
+
+        // create data adapter
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        // this will query your database and return the result to your datatable
+        da.Fill(_dt);
+        conn.Close();
+        da.Dispose();
+        return _dt.Rows.Count.ToString();
+
+
+    }
+
+    }
+
--- DrugsDictionaryProj/Form1.cs
+++ DrugsDictionaryProj/Form1.cs
@@ -16,5 +16,10 @@ namespace DrugsDictionaryProj
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           MessageBox .Show (   HelperClass. Imprting_Data());
+        }
     }
 }
